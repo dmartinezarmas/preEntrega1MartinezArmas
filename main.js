6224373d56418484e30dad04d9408a8a42c62365
diego_martinez_armas <134106804+dmartinezarmas@users.noreply.github.com>
2023-11-29 20:48:16 -0300

    agregado de modificaciones al DOM

--- main.js
+++ main.js
@@ -25,6 +25,43 @@ while (!array.some((Lista)=> Lista.genero == elect)) {
 }
 alert(`Hay coincidencia para: ${elect.toUpperCase()}. Revisa la consola para ver tu lista de recomendaciones`)
 console.log(array.find ((Lista)=> Lista.genero == elect))
+console.log(array[1].genero)
+let divTitle = document.getElementById("title")
+divTitle.innerHTML = "<h1>BIENVENIDO A NUESTRO SITIO DE RECOMENDACIONES</h1>"
+let divBotonInicio = document.getElementById("botonInicio")
+divBotonInicio.innerHTML = `<input id="botonIniciar" type="button" value="INICIAR">`
+let botonEscuchar = document.getElementById("botonIniciar")
+botonEscuchar.addEventListener("click", Iniciar)
+function Iniciar(){
+    let divGeneros = document.getElementById("divGenero")
+    for (let i = 0; i < 5; i++) {
+        let but = document.createElement("input")
+        but.type = "button"
+        but.value = `${array[i].genero}`
+        but.className = `but${i}`
+        divGeneros.appendChild(but)
+      
+    }
+}
+
 }
 
 Listas(eleccion)
+
+let nova = document.getElementsByTagName("input")
+nova.addEventListener("click", Sugerir)
+function Sugerir(){
+    console.log("pepera")
+    // for (let i = 0; i < 7; i++) {
+    //     let li = document.createElement("li")
+    //     li.innerHTML = `${array[i]}`
+    //     but0.appendChild(li)
+      
+    // }
+
+}
+
+
+// let sugerencias = document.getElementById("options")
+// for (const sugerencia of sugerencias){
+
